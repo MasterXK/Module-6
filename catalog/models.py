b10ff909c8e95397cf70eb1bc37da24070dc1387
vavil <vavilon164@yandex.ru>
2024-03-20 20:05:16 +0300

    Made first migration

--- catalog/models.py
+++ catalog/models.py
@@ -5,10 +5,10 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Имя')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(verbose_name='Изображение')
-    category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
-    creation_date = models.DateField(verbose_name='Дата создания')
-    last_change_date = models.DateField(verbose_name='Дата изменения')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата изменения')
 
     def __str__(self):
         return f'продукт'
